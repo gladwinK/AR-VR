3d37bec1dce31c4edc43d3468ebb8bf0d6069b99
Gladwin Kurian <gladwinkurian@gmail.com>
2022-03-04 23:25:00 +0530

    removed git-cache on local machine to get rid of the nested git folders

---
+++ Practicals/4_World_Tracking_with_add_node_and_reset_node_buttons/World Tracking/ViewController.swift
@@ -0,0 +1,47 @@
+//
+// ViewController.swift
+// World Tracking
+//
+// Created by GLADWIN KURIAN on 25/02/22.
+// Copyright (c) 2020 OWNER. All rights reserved.
+//
+// FURTHER, IF ANY
+//
+
+
+import UIKit
+import ARKit
+class ViewController: UIViewController {
+
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    let config = ARWorldTrackingConfiguration()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+       
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        self.sceneView.session.run(config)
+    }
+
+    @IBAction func Add(_ sender: Any) {
+        let node = SCNNode()
+        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+		node.geometry?.firstMaterial?.diffuse.contents = UIColor.green
+		node.position = SCNVector3(0,0,0)
+		self.sceneView.scene.rootNode.addChildNode(node)
+	}
+    
+	@IBAction func reset(_ sender: Any) {
+		self.sceneView.session.pause()
+		
+		self.sceneView.scene.rootNode.enumerateChildNodes{ (node, _) in
+				   node.removeFromParentNode()
+				   
+			   }
+		self.sceneView.session.run(config, options: [.resetTracking, .removeExistingAnchors])
+			
+	}
+}
+
